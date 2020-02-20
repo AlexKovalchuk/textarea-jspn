a19f9315ed5f4e601ad342f9a3fc2d58c6f5e520
OleksandrKovalchuk <alexkovalchuk1991@gmail.com>
2020-02-20 11:09:33 +0200

    installed libs for redux

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React TypeScript 
+          Alex is Learning React TypeScript like a Boss.
         </a>
       </header>
     </div>

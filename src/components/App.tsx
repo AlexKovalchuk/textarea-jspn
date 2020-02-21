5453c192e5db9bc17b473a4108782a4ea65c97bf
OleksandrKovalchuk <alexkovalchuk1991@gmail.com>
2020-02-21 12:00:59 +0200

    started to wortk with my task

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const App = (props: any) => {
+
+    return (
+        <div className="home-page-container">
+            <h1>Home Component</h1>
+        </div>
+    )
+}
+
+export default App;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import createHistory from "history/createBrowserHistory";
-import HomePage from "../components/HomePage";
+import App from "../components/App";
 
 export const history = createHistory();
 
@@ -11,7 +11,7 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={HomePage} />
+        <Route path="/" component={App} />
       </Switch>
     </div>
   </Router>

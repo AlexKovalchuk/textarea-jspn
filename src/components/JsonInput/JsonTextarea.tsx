2edb7f0886821a1e2ca8305f422d30491bb83fe4
OleksandrKovalchuk <alexkovalchuk1991@gmail.com>
2020-02-26 17:07:42 +0200

    edited eslint and fixed errors

--- src/components/JsonInput/JsonTextarea.tsx
+++ src/components/JsonInput/JsonTextarea.tsx
@@ -1,16 +1,18 @@
 import React, { FunctionComponent, useState, useEffect, useRef } from 'react';
 import './JsonTextareaStyle.css';
 
-interface ParentStateItem {
-    optionList: Array<object>
+interface ParentPropsItem {
+    optionList: { title: string }[]
 }
 
-const JsonInput:FunctionComponent<ParentStateItem> = (props: any) => {
+const JsonInput:FunctionComponent<ParentPropsItem> = props => {
     const {optionList} = props;
-    const [jsonText, setJsonText] = useState('unset');
     // const [jsonArray, setJsonArray] = useState<Array<any>>([]);
+    const [jsonText, setJsonText] = useState('unset');
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [autocomplite, setAutocomplite] = useState([]);
     const [selection, setSelection] = useState(0);
-    let textareaElementRef = useRef<HTMLTextAreaElement>(null);
+    const textareaElementRef = useRef<HTMLTextAreaElement>(null);
 
     const changeCursorePosition = () => {
         if(textareaElementRef && textareaElementRef.current) {
@@ -19,12 +21,12 @@ const JsonInput:FunctionComponent<ParentStateItem> = (props: any) => {
         }
     }
 
-    const convertObjectToString = (data: any) => {
+    const convertObjectToString = (data: object[] | object) => {
         return JSON.stringify(data, undefined, 2)
     }
 
     const checkIsOnlyLetters = (str: string): boolean => {
-        var letters = /^[A-Za-z]+$/;
+        const letters = /^[A-Za-z]+$/;
         if(str.match(letters)) return true
         else return false
     }
@@ -42,8 +44,21 @@ const JsonInput:FunctionComponent<ParentStateItem> = (props: any) => {
     }, [])
 
     const textAreaOnChangeHandler = (element: React.FormEvent<HTMLTextAreaElement>): void => {
-        setSelection(textareaElementRef.current!.selectionStart);
-        setJsonText(element.currentTarget.value);
+        const selectionStart = textareaElementRef.current!.selectionStart
+        const newValue = element.currentTarget.value
+        setSelection(selectionStart);
+        setJsonText(newValue);
+        findMatchesOptionsTitles(getLastTypedWord(selectionStart, newValue));
+    }
+
+    const getLastTypedWord = (selectionStart:  number, text: string): string => {
+        const substringStart = text.substring(0, selectionStart).split(' ');
+        // console.log('substringStart', substringStart);
+        return substringStart[substringStart.length-1];
+    }
+
+    const findMatchesOptionsTitles = (text: string): object[] => {
+        return  optionList.filter(option => option.title!.substring(0, text.length).toLowerCase() === text.toLowerCase());
     }
 
     const clearStringJson = (str: string): string => {
@@ -51,30 +66,17 @@ const JsonInput:FunctionComponent<ParentStateItem> = (props: any) => {
             if(!checkIsOnlyLetters(currentValue)) return accumulate += currentValue;
             return accumulate;
         });
-    } 
+    }
 
-
-    
-    const onKeyPressed = (event:  any): void => {
+    const onKeyPressed = (event: React.KeyboardEvent<HTMLTextAreaElement>): void => {
         if (event.keyCode === 9) { // tab was pressed
             event.preventDefault();
-            let value = '';
-            const { selectionStart, selectionEnd } = event.target
-            const substringStart = jsonText.substring(0, selectionStart).split(' ');
-            const substringTab = substringStart[substringStart.length-1];
-            const findedTitles: Array<any> = optionList.filter((option: { title: React.ReactNode }) => option.title === substringTab)
-            if(findedTitles && findedTitles.length) {
-                console.log('if', findedTitles);
-                value = clearStringJson(jsonText.substring(0, selectionStart - findedTitles[0].title.length) + `${convertObjectToString(findedTitles[0])},` + jsonText.substring(selectionEnd))
-                setJsonText(convertObjectToString(JSON.parse(value)));                
-            }
-            else {
-                console.log('else', substringTab, ' - ', findedTitles);
-                value = jsonText.substring(0, selectionStart) + '  ' + jsonText.substring(selectionEnd);
-                setJsonText(value);
-            }
-            
-            setSelection(selectionStart + 1); 
+            const { selectionStart, selectionEnd } = event.target as HTMLTextAreaElement;
+            const substringTab = getLastTypedWord(selectionStart, jsonText);
+            const findedTitles: {title: string} | undefined = optionList.find((option: {title: string}) => option.title === substringTab);
+            if (findedTitles) setJsonText(convertObjectToString(JSON.parse(clearStringJson(jsonText.substring(0, selectionStart - findedTitles.title.length) + `${convertObjectToString(findedTitles)},` + jsonText.substring(selectionEnd)))));
+            else setJsonText(jsonText.substring(0, selectionStart) + '  ' + jsonText.substring(selectionEnd));
+            setSelection(selectionStart + 1);
         }
     }
 
@@ -85,21 +87,21 @@ const JsonInput:FunctionComponent<ParentStateItem> = (props: any) => {
             )
         });
     }
-    
+
     return (
         <div className="json-textarea-container">
             <h1>Input Json component</h1>
             <div className="json-textarea-block">
                 <div className="block-one">
-                    <textarea 
+                    <textarea
                         ref={textareaElementRef}
-                        rows={40} 
-                        cols={125} 
-                        className="json-textarea" 
+                        rows={40}
+                        cols={125}
+                        className="json-textarea"
                         value={jsonText}
                         tabIndex={0}
                         onKeyDown={onKeyPressed}
-                        onChange={(e: React.FormEvent<HTMLTextAreaElement>) => textAreaOnChangeHandler(e)}
+                        onChange={textAreaOnChangeHandler}
                     />
                 </div>
                 <div className="block-two">
